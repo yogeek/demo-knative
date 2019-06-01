e5c56fbead57302de2894c3a923a69f38ef69df2
Guillaume Dupin <gdupin@gmail.com>
2019-06-01 02:13:50 +0200

    fix minikube script to add addon after cluster creation

--- minikube.sh
+++ minikube.sh
@@ -8,9 +8,6 @@ mkdir -p $HOME/.minikube
 touch $HOME/.kube/config
 export KUBECONFIG=$HOME/.kube/config 
 
-sudo minikube addons enable heapster
-sudo minikube addons enable metrics-server
-
 
 # https://istio.io/docs/setup/kubernetes/prepare/platform-setup/minikube/
 # https://github.com/kubernetes/minikube/blob/master/docs/vmdriver-none.md#known-issues
@@ -18,5 +15,8 @@ sudo -E minikube start --vm-driver=none \
 --memory=8192 --cpus=4 \
 --extra-config=apiserver.enable-admission-plugins="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook" \
 --extra-config=kubelet.resolv-conf=/run/systemd/resolve/resolv.conf
+
+sudo minikube addons enable heapster
+sudo minikube addons enable metrics-server
 
 watch kubectl get pods --namespace kube-system
